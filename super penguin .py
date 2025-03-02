f1921dc91105e14b4bbb13f74d4814310828194a
azeem1234coder <odusanyaabdulazeem25@gmail.com>
2025-03-02 11:56:24 -0800

    44

---
+++ super penguin .py
@@ -0,0 +1,24 @@
+class Bird:
+    def __init__(self):
+        print("Bird is ready")
+    def whoisthis(self):
+       print("Bird")
+    def swim(self):
+        print("swim faster")
+class penguin(Bird):
+    def __init__(self):
+        super().__init__()
+        print("Penguin is ready")
+    def whoisthis(self):
+        print("Penguin")
+    def run(self):
+        print("run faster")
+peggy = penguin()
+peggy.whoisthis()
+peggy.swim()
+peggy.run()
+    
+      
+    
+    
+    
